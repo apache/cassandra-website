7494d96c9afb437bfe0a061b16efbfc6d7cfae45
eevans <eevans@13f79535-47bb-0310-9956-ffa450edef68>
2010-04-18 23:07:24 +0000

    update site for new stable release
    git-svn-id: http://svn.apache.org/repos/asf/cassandra/site@935429 13f79535-47bb-0310-9956-ffa450edef68

--- src/settings.py
+++ src/settings.py
@@ -92,8 +92,8 @@ SITE_POST_PROCESSORS = {
 }
 
 class CassandraDef(object):
-    stable_version = '0.6.0'
-    stable_release_date = '2010-04-12'
+    stable_version = '0.6.1'
+    stable_release_date = '2010-04-18'
     devel_version = '0.6.0-rc1'
     devel_release_date = '2010-03-31'
     devel_exists = False
