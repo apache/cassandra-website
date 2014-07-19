77d471e35819014d5b24f70678e5b4f846390573
eevans <eevans@13f79535-47bb-0310-9956-ffa450edef68>
2014-07-19 20:56:27 +0000

    update versioning for 2.1.0-rc4 release
    git-svn-id: http://svn.apache.org/repos/asf/cassandra/site@1611962 13f79535-47bb-0310-9956-ffa450edef68

--- src/settings.py
+++ src/settings.py
@@ -100,8 +100,8 @@ class CassandraDef(object):
     veryoldstable_exists = True
     stable_version = '2.0.9'
     stable_release_date = '2014-06-30'
-    devel_version = '2.1.0-rc3'
-    devel_release_date = '2014-07-10'
+    devel_version = '2.1.0-rc4'
+    devel_release_date = '2014-07-19'
     devel_exists = True
     _apache_base_url = 'http://www.apache.org'
     _svn_base_url = 'https://svn.apache.org/repos/asf'
