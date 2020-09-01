1c217a9206edc042553bfa7b14010c2f361488c6
mck <mck@apache.org>
2020-09-01 20:47:29 +0200

    Cassandra website is building/including the wrong versioned nodetool docs
     patch by Mick Semb Wever; reviewed by XXX for CASSANDRA-16093

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,36 +7,44 @@ export CASSANDRA_DIR="${BUILD_DIR}/cassandra"
 
 jekyll --version
 
-# Make sure we have the latest commit of Cassandra trunk
-cd ${CASSANDRA_DIR}
-git checkout trunk
-git pull --rebase --prune
+cd ${CASSANDRA_SITE_DIR}/src
 
 # Now make the docs for the latest version
-cd ${CASSANDRA_SITE_DIR}/src
-make add-latest-doc
-
-
-# Make sure we have the latest commit of Cassandra 3.11
 pushd ${CASSANDRA_DIR}
-ant realclean
+git clean -xdff
+git checkout trunk
+git pull --rebase --prune
+popd
+make .build-doc .latest-doc-link
+
+
+# Uncomment for building docs for each of the released versions on the 3.11 branch
+#
+#versions="cassandra-3.11.5 cassandra-3.11.6 cassandra-3.11.7 cassandra-3.11.8"
+#
+#for v in $versions ; do
+#  pushd ${CASSANDRA_DIR}
+#  git clean -xdff
+#  git checkout $v
+#  popd
+#  make .build-doc
+#done
+
+# Now make the docs for 3.11
+pushd ${CASSANDRA_DIR}
+git clean -xdff
 git checkout cassandra-3.11
 git pull --rebase --prune
 popd
-
-# Now make the docs for 3.11
-make .build-doc
+make .build-doc .stable-doc-link
 
 # Relink the 3.11 version
 LATEST_VERSION=$(basename $(find ./doc -iname 3.11* -type d | sort | tail -n 1))
 rm -f doc/3.11
 ln -s -f ${LATEST_VERSION} doc/3.11
 
-make build
-
-
 # Generate the rest of the site
-make
+make build
 
 # Fix the links in the resource paths for the landing pages of each version in the publish directory
 cd ${CASSANDRA_SITE_DIR}
