683b2f4b3e3dc206943b0af2fd38f35fa782407f
Anthony Grasso <anthony@thelastpickle.com>
2021-04-16 05:53:39 +1000

    CASSANDRA-16066: update to run.sh script
    * Bug fixes
    * Completed website preview function
    * Completed website build function

--- run.sh
+++ run.sh
@@ -118,32 +118,45 @@ Options
     * website
         -e ENV_FILE               Path defined by ENV_FILE to a docker environment file. Use this to override the values
                                   of the environment variables in the Docker website container.
-        -l LOCAL_CASSANDRA_REPO   Path defined by LOCAL_CASSANDRA_REPO to local Cassandra repository. Use this option if
-                                  you want to use your local checkout of the Cassandra repository instead of the one
-                                  inside the Docker website container.
+
         -c CONTAINER_TAG          Tag defined by CONTAINER_TAG of the website container.
-        -v REPOSITORY:VERSIONS    List of versions defined by VERSIONS for a particular repository defined by
-                                  REPOSITORY. The valid values for REPOSITORY are 'cassandra' and 'cassandra-website'.
-                                  The values for VERSIONS are split by comma with no spaces. The repository and list of
-                                  versions are split by a colon ':'. For example:
-                                    -v cassandra:trunk,my_test_branch_311,my_test_branch_40
-        -t REPOSITORY:TAGS        List of tags defined by TAGS for a particular repository defined by REPOSITORY.
-                                  The valid values for REPOSITORY are 'cassandra' and 'cassandra-website'. The values
-                                  for TAGS are split by comma with no spaces. The repository and list of tags are split
-                                  by a colon ':'. For example:
+
+        -b REPOSITORY:BRANCHES    List of branches defined by BRANCHES in a repository defined by REPOSITORY that will
+                                  be used as the source content to generate the docs and/or website. This option can be
+                                  specified multiple times. The valid values for REPOSITORY are only 'cassandra' and
+                                  'cassandra-website'. The values for BRANCHES are split by comma with no spaces. The
+                                  repository and list of branches are split by a colon ':'. For example:
+                                    -b cassandra:trunk,my_test_branch_311,my_test_branch_40
+
+        -t REPOSITORY:TAGS        List of tags defined by TAGS in a repository defined by REPOSITORY that will be used
+                                  as the source content to generate the docs and/or website. This option can be
+                                  specified multiple times. The valid values for REPOSITORY are only 'cassandra' and
+                                  'cassandra-website'. The values for TAGS are split by comma with no spaces. The
+                                  repository and list of tags are split by a colon ':'. For example:
                                     -t cassandra:cassandra-4.0,cassandra-3.11,cassandra-3.0
-        -u REPOSITORY:URL         The location defined by URL of a particular repository defined by REPOSITORY.
-                                  The valid values for REPOSITORY are 'cassandra' and 'cassandra-website'. The URL can
-                                  be a HTTP git url or a file location on your local host. The repository and location
-                                  are split by a colon ':'. For example:
-                                    -s cassandra-website:~/work/cassandra-website
-        -b BUILD_ARG:VALUE        A container build argument defined by BUILD_ARG that overrides the default value with
+
+        -u REPOSITORY:URL         The location defined by URL of the repository defined by REPOSITORY that will be used
+                                  as the source content to generate the docs and/or website. This option can be
+                                  specified multiple times. The valid values for REPOSITORY are only 'cassandra' and
+                                  'cassandra-website'. The URL can be a HTTP git url or a file location on your local
+                                  host. The repository and location are split by a colon ':'. For example:
+                                    -u cassandra-website:~/local/path/to/cassandra-website
+                                    -u cassandra:https://github.com/myfork/cassandra.git
+
+        -z UI_BUNDLE_ZIP_URL      The UI bundle ZIP location defined by UI_BUNDLE_ZIP_URL that will be used to style the
+                                  website when it is built. The value be a HTTP url or a file location on your local
+                                  host.  For example:
+                                    -z ~/local/path/to/ui-bundle.zip
+
+        -v BUILD_ARG:VALUE        A container build argument defined by BUILD_ARG that overrides the default value with
                                   the value for VALUE. This option can be specified multiple times and is only applied
                                   when using the 'build' command. This option is ignored in all other cases. The build
                                   argument and value are split by a colon ':'. For example:
-                                    -b CASSANDRA_REPOSITORY_URL_ARG:https://github/user/my_cassandra_fork.git
+                                    -v CASSANDRA_REPOSITORY_URL_ARG:https://github/user/my_cassandra_fork.git
+
         -g                        Flag to enable generation of versioned docs when running website 'build', and
                                   'preview'. This option is ignored in all other cases.
+
         -a                        Flag to disable automatically committing the generated docs to the Cassandra
                                   repository when using a single branch. Use this option when you use your local
                                   checkout of the Cassandra repository and want to commit any newly generated AsciiDoc
@@ -157,31 +170,31 @@ EOF
 
 
 parse_website_command_options() {
-  while getopts "e:l:c:v:t:u:b:gah" opt_flag; do
+  while getopts "e:c:b:t:u:z:v:gah" opt_flag; do
     case $opt_flag in
       e)
         env_file=$OPTARG
       ;;
-      l)
-        local_cassandra_repository_path=$OPTARG
-      ;;
       c)
         container_tag=$OPTARG
       ;;
-      v)
-        repository_versions=$OPTARG
+      b)
+        repository_branches+=("$OPTARG")
       ;;
       t)
-        repository_tags=$OPTARG
+        repository_tags+=("$OPTARG")
       ;;
       u)
-        repository_url=$OPTARG
+        repository_url+=("$OPTARG")
       ;;
-      b)
-        container_build_args+=($OPTARG)
+      z)
+        ui_bundle_zip_url="$OPTARG"
+      ;;
+      v)
+        container_build_args+=("$OPTARG")
       ;;
       g)
-        cmd_generate_versioned_docs="run"
+        command_generate_docs="generate-docs"
       ;;
       a)
         automatically_commit_generated_version_docs="disabled"
@@ -189,52 +202,206 @@ parse_website_command_options() {
       h)
         usage
       ;;
+      *)
+        usage
+      ;;
     esac
 done
 }
 
 
+get_source_location_information() {
+  local location_source
+
+  location_source=$1
+  location_type=""
+
+  if [ "$(cut -d'/' -f1 <<< "${location_source}")" = "~" ]
+  then
+    location_source="${location_source//\~/$HOME}"
+  fi
+
+  if [ -d "${location_source}" ]
+  then
+    location_type="dir"
+  elif [ -f "${location_source}" ]
+  then
+    location_type="file"
+  else
+    case $(cut -d':' -f1 <<< "${location_source}") in
+      http | https)
+        location_type="url"
+      ;;
+      file)
+        location_source=$(sed 's/^file:\/\///g' <<< "${location_source}")
+        if [ -d "${location_source}" ]
+        then
+          location_type="dir"
+        elif [ -f "${location_source}" ]
+        then
+          location_type="file"
+        else
+          location_type="unknown"
+        fi
+      ;;
+      *)
+        location_type="unknown"
+      ;;
+    esac
+  fi
+
+  echo "${location_type}:${location_source}"
+}
+
+
+exec_docker_command() {
+  echo
+  echo "Executing docker command: 'docker $1'"
+#  eval "docker $1"
+}
+
+
 build_website_container() {
   local docker_build_args
-  for key_value in ${container_build_args[@]}
+  for key_value in ${container_build_args[*]}
   do
     docker_build_args+=(--build-arg "$(sed '0,/:/ s/:/=/' <<< "${key_value}")")
   done
 
-  docker build -f ./Dockerfile -t ${container_tag} ${docker_build_args[@]} ./
+  exec_docker_command "build -f ./Dockerfile -t ${container_tag} ${docker_build_args[*]} ./"
 }
 
 
 build_website_ui_container() {
-  docker build -f ./site-ui/Dockerfile -t ${container_tag} ./site-ui/
+  exec_docker_command "build -f ./site-ui/Dockerfile -t ${container_tag} ./site-ui/"
+}
+
+
+set_antora_url_source() {
+  local antora_env_name
+  local antora_env_value
+
+  local url_source_name
+  local url_source_value
+
+  local location_type
+  local location_source
+
+  antora_env_name="$1"
+  url_source_name="$2"
+  url_source_value="$3"
+
+  antora_env_value="${url_source_value}"
+
+  location_info=$(get_source_location_information "${url_source_value}")
+  location_type=$(sed '0,/:/s/:/=/' <<< "${location_info}" | cut -d'=' -f1)
+  location_source=$(sed '0,/:/s/:/=/' <<< "${location_info}" | cut -d'=' -f2)
+
+  if [ "${location_type}" = "dir" ] || [ "${location_type}" = "file" ]
+  then
+    if [ "${url_source_name}" = "cassandra-website" ]
+    then
+      cassandra_website_source_set="true"
+    fi
+    antora_env_value="/home/build/${url_source_name}"
+    vol_args+=("-v ${location_source}:${antora_env_value}")
+  fi
+
+  env_args+=("-e ${antora_env_name}=${antora_env_value}")
+}
+
+
+run_docker_website_command() {
+  local container_command
+
+  container_command="$1"
+
+  local port_map_option=""
+
+  if [ "${command}" = "preview" ]
+  then
+    port_map_option="-p 5151:5151/tcp"
+  fi
+
+  local env_file_arg
+  local env_args
+  local vol_args
+
+  local repository_name
+  local repository_value
+  local location_type
+
+  local antora_content_source_env_name
+
+  local cassandra_website_source_set
+
+
+  if [ -z "$(docker images -q "${container_tag}")" ]
+  then
+    build_website_container
+  fi
+
+  if [ -f "${env_file}" ]
+  then
+    env_file_arg="--env-file ${env_file}"
+  fi
+
+  env_args=()
+  vol_args=()
+
+  # We want to handle the following argument inputs and convert them to their equivalent ANTORA_CONTENT_SOURCES docker
+  # environment variable.
+  # cassandra:trunk,my_test_branch_311,my_test_branch_40 -> ANTORA_CONTENT_SOURCES_CASSANDRA_BRANCHES=trunk,my_test_branch_311,my_test_branch_40
+  # cassandra:cassandra-4.0,cassandra-3.11,cassandra-3.0 -> ANTORA_CONTENT_SOURCES_CASSANDRA_TAGS=cassandra-4.0,cassandra-3.11,cassandra-3.0
+  # cassandra:https://github.com/myfork/cassandra.git -> ANTORA_CONTENT_SOURCES_CASSANDRA_URL=https://github.com/myfork/cassandra.git
+  #
+  # We can do this by iterating through each of the repository source arrays and splitting the repository name from its
+  # supplied value. The eval command for the inner for loop resolves to "${repository_<type>[*]}". This combined with
+  # the outer for loop means we will process all the values in arrays: repository_branches, repository_tags, repository_url.
+  cassandra_website_source_set="false"
+  for repository_source_type in branches tags url
+  do
+    for repository_source in $(eval echo \$\{"repository_${repository_source_type}"\[\*\]\})
+    do
+      repository_name=$(sed '0,/:/s/:/=/' <<< "${repository_source}" | cut -d'=' -f1)
+      repository_value=$(sed '0,/:/s/:/=/' <<< "${repository_source}" | cut -d'=' -f2)
+
+      antora_content_source_env_name="ANTORA_CONTENT_SOURCES_$(tr '[:lower:]' '[:upper:]' <<< "${repository_name}" | sed 's/\-/_/g')_$(tr '[:lower:]' '[:upper:]' <<< "${repository_source_type}")"
+
+      if [ "${repository_source_type}" = "url" ]
+      then
+        set_antora_url_source "${antora_content_source_env_name}" "${repository_name}" "${repository_value}"
+      else
+        env_args+=("-e ${antora_content_source_env_name}=${repository_value}")
+      fi
+    done
+  done
+
+  if [ "${cassandra_website_source_set}" = "false" ]
+  then
+    vol_args+=("-v $(pwd):/home/build/cassandra-website")
+  fi
+
+  env_args+=("-e CREATE_GIT_COMMIT_WHEN_GENERATING_DOCS=${automatically_commit_generated_version_docs}")
+
+  if [ -n "${ui_bundle_zip_url}" ]
+  then
+    ui_bundle_name=$(rev <<< "${ui_bundle_zip_url}" | cut -d'/' -f1 | rev)
+
+    set_antora_url_source "ANTORA_UI_BUNDLE_URL" "${ui_bundle_name}" "${ui_bundle_zip_url}"
+  fi
+
+  exec_docker_command "run -i -t ${port_map_option} ${vol_args[*]} ${env_file_arg} ${env_args[*]} ${container_tag} ${command_generate_docs} ${container_command}"
+}
+
+
+run_website_build() {
+  run_docker_website_command "build-site"
 }
 
 
 run_website_preview() {
-  local env_file_arg
-  local env_args
-  local cassandra_volume_arg
-
-  if [ -n "${env_file}" ]
-  then
-    env_file_arg="--env-file .env.dev"
-  fi
-
-  if [ -n "${local_cassandra_repository_path}" ]
-  then
-    cassandra_volume_arg="-v ${local_cassandra_repository_path}:/home/build/cassandra"
-  fi
-
-  env_args=(-e CMD_PREVIEW="run")
-  env_args+=(-e CMD_GENERATE_DOCS="${cmd_generate_versioned_docs}")
-  env_args+=(-e GENERATE_DOCS_COMMIT_CHANGES_TO_LOCAL="${automatically_commit_generated_version_docs}")
-
-  docker run -i -t -p 5151:5151/tcp -v "$(pwd)":/home/build/cassandra-website ${cassandra_volume_arg} ${env_file_arg} ${env_args[@]} ${container_tag}
-
-
-#docker run -i -t -p 5151:5151 -v $(pwd):/home/build/cassandra-website --env-file .env.dev cassandra-website_website-dev:latest
-#docker run -i -t -p 5151:5151 -v $(pwd):/home/build/cassandra-website -v $(pwd)/../cassandra:/home/build/cassandra --env-file .env.dev cassandra-website_website-dev:latest
-#docker run -i -t -p 5151:5151 -v $(pwd):/home/build/cassandra-website -v $(pwd)/../cassandra:/home/build/cassandra -e PREVIEW_MODE=enabled cassandra-website_website-dev:latest
+  run_docker_website_command "preview"
 }
 
 
@@ -254,15 +421,14 @@ run_website_command() {
     ;;
 
     docs)
-      debug "Generates the documentation the Cassandra versions defined."
+      debug "Generates the documentation for the specified Cassandra versions."
     ;;
 
     build)
-      debug "Build the website using Antora."
+      run_website_build
     ;;
 
     preview)
-      debug "Launch a server that monitors the site content and renders the site when a change is detected."
       run_website_preview
     ;;
   esac
@@ -304,7 +470,7 @@ run_website_ui_command() {
         port_map_option="-p 5252:5252/tcp"
       fi
 
-      docker run -i -t ${port_map_option} -v "$(pwd)"/site-ui/:/home/site-ui ${container_tag} "$@"
+      exec_docker_command "run -i -t ${port_map_option} -v $(pwd)/site-ui/:/home/site-ui ${container_tag} $*"
     ;;
   esac
 
@@ -320,10 +486,11 @@ env_file=""
 local_cassandra_repository_path=""
 container_tag=""
 container_build_args=()
-repository_versions=""
-repository_tags=""
-repository_url=""
-cmd_generate_versioned_docs=""
+repository_branches=()
+repository_tags=()
+repository_url=()
+ui_bundle_zip_url=""
+command_generate_docs=""
 automatically_commit_generated_version_docs="enabled"
 
 
@@ -333,7 +500,7 @@ then
 fi
 
 parse_website_command_options "$@"
-shift $(($OPTIND - 1))
+shift $((OPTIND - 1))
 
 if [ "$#" -eq 0 ]
 then
@@ -354,11 +521,12 @@ case "${arg_component}" in
     debug "env_file: ${env_file}"
     debug "local_cassandra_repository_path: ${local_cassandra_repository_path}"
     debug "container_tag: ${container_tag}"
-    debug "container_build_args: ${container_build_args[@]}"
-    debug "repository_versions: ${repository_versions}"
-    debug "repository_tags: ${repository_tags}"
-    debug "repository_url: ${repository_url}"
-    debug "cmd_generate_versioned_docs: ${cmd_generate_versioned_docs}"
+    debug "container_build_args: ${container_build_args[*]}"
+    debug "repository_branches: ${repository_branches[*]}"
+    debug "repository_tags: ${repository_tags[*]}"
+    debug "repository_url: ${repository_url[*]}"
+    debug "ui_bundle_zip_url: ${ui_bundle_zip_url}"
+    debug "command_generate_docs: ${command_generate_docs}"
     debug "automatically_commit_generated_version_docs: ${automatically_commit_generated_version_docs}"
 
     run_website_command "$@"
@@ -371,5 +539,3 @@ case "${arg_component}" in
   *)
     usage
 esac
-
-
