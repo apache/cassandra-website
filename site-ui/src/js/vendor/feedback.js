a041b9a782568d2f2122a9db7d027bc7dc9627ce
Paul Thomas Au <paul@traverstodd.com>
2021-06-09 17:09:57 -0700

    CASSANDRA-16066: Add new website content and styling
    This commit adds the content files and styling resources to render the new
    website.
    
    The content added are ADOC files and associated images to the following
    directory. It is placed in this directory to be consumed by Antora which is used to
    generate the website HTML.
    
        site-content/source/modules/ROOT/pages/
    
    The styling resources added are CSS, JS, layouts, and partials to the following
    directory. It is placed in this directory to be consumed by gulp which will
    package the resources into a ZIP file. The ZIP file is then passed to Antora
    when the site is rendered.
    
        site-ui/src/
    
    patch by Paul Au; reviewed by Anthony Grasso, Erick Ramirez, Mick Semb Wever for CASSANDRA-16762

--- site-ui/src/js/vendor/feedback.js
+++ site-ui/src/js/vendor/feedback.js
@@ -4,7 +4,7 @@
   if (Math.max(window.screen.availHeight, window.screen.availWidth) < 769) return
 
   window.addEventListener('load', function () {
-    var config = document.getElementById('feedback-script').dataset
+    // var config = document.getElementById('feedback-script').dataset
     var script = document.createElement('script')
     script.src = 'https://issues.apache.org/jira/projects/CASSANDRA/issues'
     document.body.appendChild(script)
