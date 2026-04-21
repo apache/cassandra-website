083a72470a2ddd9b5228b5265717b86c6f2a6fd1
Stefan Miklosovic <smiklosovic@apache.org>
2026-04-21 13:19:39 +0200

    Revert "Upgrade site-ui to antora 3, and upgrading other components"
    This reverts commit 569057c649b6b113d6c900b69f6ace1093ff3a62.

--- site-ui/gulp.d/tasks/build.js
+++ site-ui/gulp.d/tasks/build.js
@@ -6,6 +6,7 @@ const buffer = require('vinyl-buffer')
 const concat = require('gulp-concat')
 const cssnano = require('cssnano')
 const fs = require('fs-extra')
+const imagemin = require('gulp-imagemin')
 const merge = require('merge-stream')
 const ospath = require('path')
 const path = ospath.posix
@@ -95,7 +96,16 @@ module.exports = (src, dest, preview) => () => {
       .src(['css/site.css', 'css/vendor/docsearch.css'], { ...opts, sourcemaps })
       .pipe(postcss((file) => ({ plugins: postcssPlugins, options: { file } }))),
     vfs.src('font/*.{ttf,woff*(2)}', opts),
-    vfs.src('img/**/*.{gif,ico,jpg,png,svg}', opts),
+    vfs
+      .src('img/**/*.{gif,ico,jpg,png,svg}', opts)
+      .pipe(
+        imagemin([
+          imagemin.gifsicle(),
+          imagemin.jpegtran(),
+          imagemin.optipng(),
+          imagemin.svgo({ plugins: [{ removeViewBox: false }] }),
+        ])
+      ),
     vfs.src('helpers/*.js', opts),
     vfs.src('layouts/*.hbs', opts),
     vfs.src('partials/*.hbs', opts)
